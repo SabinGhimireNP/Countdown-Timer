8d460a4ee7c6224fcb9d9258c1f6aa6a3f0fa1db
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-14 22:05:26 +0545

    Polishing

--- scripts.js
+++ scripts.js
@@ -1,52 +1,65 @@
-let countdown;
+// Main variables Declaration
 const timerDisplay = document.querySelector(".display__time-left");
 const endTime = document.querySelector(".display__end-time");
 const timeSet = document.querySelectorAll("[data-time]");
+const form = document.customForm;
+
+let countdown;
+
+//Fuunction Declaration
+
+//main timer function
 function timer(seconds) {
   clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
+  
   displayEndTime(then);
   displayTimeLeft(seconds);
-  //   console.log(now);
+
   countdown = setInterval(() => {
     const secLeft = Math.round((then - Date.now()) / 1000);
+
     if (secLeft < 0) {
       clearInterval(countdown);
       return;
     }
+
     displayTimeLeft(secLeft);
   }, 1000);
 }
 
+// Display the main timer
 function displayTimeLeft(seconds) {
   const min = Math.floor(seconds / 60);
   const sec = seconds % 60;
   const display = `${min}:${sec < 10 ? "0" : ""}${sec}`;
+
   timerDisplay.textContent = display;
-  console.log({ display });
 }
 
+//Displays remaiing time
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hr = end.getHours();
   const min = end.getMinutes();
 
-  endTime.textContent = `Be back at: ${hr > 12 ? hr - 12 : hr}:${min} ${
-    hr > 12 ? "PM" : "AM"
-  }`;
+  endTime.textContent = `Be back at: ${hr > 12 ? hr - 12 : hr}:${ min < 10 ? "0" : ""}${min} ${hr > 12 ? "PM" : "AM"}`;
 }
 
-function setTimer() {}
-
+//Handels quickAccess timers
 timeSet.forEach((time) =>
   time.addEventListener("click", () => timer(time.dataset.time))
 );
 
-document.customForm.addEventListener("submit", function (e) {
+//Handels input minute Field
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   const sec = this.minutes.value * 60;
+
   timer(sec);
+
   this.reset();
-  console.log(min);
 });
